1c7ea034e6baa09b7aa4aa0920d8b9c8d7980b42
조은영 <hdtv0218@gmail.com>
2024-05-16 09:40:11 +0900

    feat : 로그인 유도, 백준에 제출 완료 문구 띄우기

--- background/background.js
+++ background/background.js
@@ -1,39 +1,46 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log('백그라운드 실행됨');
-
     if (message.type === 'sendTable') {
         console.log('Received table data from content script:', message.tableData);
 
         chrome.cookies.get({ url: 'https://algnote.duckdns.org', name: 'access_token' }, function (cookie) {
-            if (cookie) {
-                // console.log('백그라운드 쿠키 있음', cookie.value);
-                sendDataToAPI(message.tableData, cookie.value);
+            if (cookie !== null) {
+                sendDataToAPI(message.tableData, cookie.value, sender.tab.id);
             } else {
-                const alert = console.log.bind(console);
-                alert('서비스 재로그인 후 제출 페이지를 새로고침해주세요'); // 내가 새로고침되게 만들까
+                chrome.notifications.create(
+                    {
+                        type: 'basic',
+                        iconUrl: '../images/logo.png',
+                        title: '로그인 필요',
+                        message: '서비스 로그인 후 제출 페이지를 새로고침해주세요',
+                    },
+                    function (notificationId) {
+                        if (chrome.runtime.lastError) {
+                            console.error(chrome.runtime.lastError);
+                        } else {
+                            console.log('Notification created with ID:', notificationId);
+                        }
+                    }
+                );
+                setTimeout(() => {
+                    chrome.tabs.update(sender.tab.id, { url: 'https://algnote.duckdns.org/login' });
+                }, 3000); // 3초 후에 로그인 페이지로 리디렉션
             }
         });
     }
 });
 
-function sendDataToAPI(tableData, cookie) {
+function sendDataToAPI(tableData, cookie, tabId) {
     fetch('https://algnote.duckdns.org/api/submissions', {
         method: 'POST',
         headers: {
             Authorization: `Bearer ${cookie}`,
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ tableData }),
+        body: JSON.stringify(tableData),
     })
         .then((response) => response.text())
-        .then((text) => {
-            // try{
-            //     const data=JSON.parse(text);
-            //     console.log('API 응답', data);
-            // }catch(error){
-            //     console.error('파싱 실패로 원본 텍스트 출력', text)
-            // }
-            console.log(text);
+        .then(() => {
+            chrome.tabs.sendMessage(tabId, { message: 'success' });
         })
-        .catch((error) => console.error('Error sending data to API:', error));
+        .catch((error) => chrome.tabs.sendMessage(tabId, { message: 'fail' + error.message }));
 }
