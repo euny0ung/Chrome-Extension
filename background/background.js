5c40bd4be3ddbb9a2c14bbda30570847a6b9271c
조은영 <hdtv0218@gmail.com>
2024-05-16 15:35:04 +0900

    feat : [FE] 로그인 유도방식 onclick으로 변경

--- background/background.js
+++ background/background.js
@@ -1,33 +1,66 @@
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+    if (reason === 'install') {
+        chrome.tabs.create({
+            url: 'https://algnote.duckdns.org',
+        });
+    }
+});
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === 'sendTable') {
-        /// console.log('Received table data from content script:', message.tableData);
+        console.log('Received table data from content script:', message.tableData);
 
         chrome.cookies.get({ url: 'https://algnote.duckdns.org', name: 'access_token' }, function (cookie) {
             if (cookie !== null) {
                 sendDataToAPI(message.tableData, cookie.value, sender.tab.id);
             } else {
-                chrome.notifications.create(
-                    {
-                        type: 'basic',
-                        iconUrl: '../images/small_logo.png',
-                        title: '로그인 필요',
-                        message: '서비스 로그인 후 제출 페이지를 새로고침해주세요. 3초 후 로그인 페이지로 이동합니다.',
-                    },
-                    function (notificationId) {
-                        if (chrome.runtime.lastError) {
-                            console.error(chrome.runtime.lastError);
-                        } else {
-                            console.log('Notification created with ID:', notificationId);
-                        }
-                    }
-                );
-                setTimeout(() => {
-                    chrome.tabs.update(sender.tab.id, { url: 'https://algnote.duckdns.org/login' });
-                }, 3000); // 3초 후에 로그인 페이지로 리디렉션
+                createLoginNotification(sender.tab.id, sender.url, message.tableData);
             }
         });
     }
 });
+
+function createLoginNotification(tabId, originUrl, tableData) {
+    chrome.notifications.create(
+        {
+            type: 'basic',
+            iconUrl: '../images/small_logo.png',
+            title: '로그인 필요',
+            message: '로그인이 필요합니다. 클릭시 로그인 화면으로 이동합니다.',
+        },
+        function (notificationId) {
+            if (chrome.runtime.lastError) {
+                console.error(chrome.runtime.lastError);
+            } else {
+                console.log('Notification created with ID:', notificationId);
+                chrome.notifications.onClicked.addListener(function handler(notificationId) {
+                    chrome.notifications.onClicked.removeListener(handler);
+                    chrome.tabs.create({ url: 'https://algnote.duckdns.org/login', active: true }, function (tab) {
+                        chrome.storage.local.set({
+                            redirectTabId: tabId,
+                            originUrl: originUrl,
+                            tableData: tableData,
+                        });
+                    });
+                });
+            }
+        }
+    );
+}
+
+// 로그인 후 원래 페이지로 돌아가는 로직 추가
+// chrome.webNavigation.onCompleted.addListener(
+//     function (details) {
+//         chrome.storage.local.get(['redirectTabId', 'originUrl', 'tableData'], function (data) {
+//             if (data.redirectTabId && data.originUrl && details.url === 'https://algnote.duckdns.org/') {
+//                 console.log(data.redirectTabId, data.originUrl, details.url);
+//                  chrome.tabs.update(data.redirectTabId, { url: data.originUrl }, function () {
+//                 chrome.storage.local.remove(['redirectTabId', 'originUrl', 'tableData']); // 저장된 값을 제거
+//             })
+//         });
+//     },
+//     { url: [{ urlMatches: 'https://algnote.duckdns.org/' }] }
+// );
 
 function sendDataToAPI(tableData, cookie, tabId) {
     fetch('https://algnote.duckdns.org/api/submissions', {
@@ -42,5 +75,5 @@ function sendDataToAPI(tableData, cookie, tabId) {
         .then(() => {
             chrome.tabs.sendMessage(tabId, { message: 'success' });
         })
-        .catch((error) => chrome.tabs.sendMessage(tabId, { message: 'fail' + error.message }));
+        .catch((error) => chrome.tabs.sendMessage(tabId, { message: 'fail: ' + error.message }));
 }
